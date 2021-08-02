e913264c7b52b22ae2c15a534ae6559b68e68abc
thibault <thibault.m-a@outlook.com>
2021-08-02 13:18:12 +0200

    v0.3.1

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,21 +4,21 @@ module.exports ={
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
+    "host":     process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_BASE,
+    "host":     process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_BASE,
+    "host":     process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
